46bc1ba484b7d3f75d1ddaab8fb41ce2c9c8710a
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-12-02 16:55:41 +0000

    TypeScript, Eslint and Jest Config

---
+++ src/utils/isNumber.test.ts
@@ -0,0 +1,15 @@
+import { isNumber } from './isNumber'
+
+describe('isNumber Utils', () => {
+  test('Its a number', () => {
+    [0, 1, 2, -1, 1.345e17, '1'].map((n) => {
+      return expect(isNumber(n)).toEqual(true)
+    })
+  })
+
+  test('Its not a number', () => {
+    [false, true, NaN, [], {}, '1a'].map((n) => {
+      return expect(isNumber(n)).toEqual(false)
+    })
+  })
+})
